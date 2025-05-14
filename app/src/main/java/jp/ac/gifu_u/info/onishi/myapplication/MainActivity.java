dea804a1ca0d021e6b5db9c21f0d2d37f0228a78
hyuga110 <ohinapon110@gmail.com>
2025-05-14 12:00:36 +0900

    forth commit

--- app/src/main/java/jp/ac/gifu_u/info/onishi/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/info/onishi/myapplication/MainActivity.java
@@ -1,80 +1,115 @@
 package jp.ac.gifu_u.info.onishi.myapplication;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.view.View;
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+    private SensorManager sensorManager;
+    private LocationManager locationManager;
 
-import jp.ac.gifu_u.info.onishi.myapplication.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private TextView textView; // 表示用テキスト
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
 
-        /*
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // TextViewを動的に生成して配置
+        textView = new TextView(this);
+        textView.setText("センサ情報待機中...");
+        setContentView(textView);
 
-        setSupportActionBar(binding.toolbar);
+        // センサマネージャ取得
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // 位置情報マネージャ取得
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+        // 位置情報のパーミッション確認と要求
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onResume() {
+        super.onResume();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // 明るさセンサ
+        Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (lightSensor != null) {
+            sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
 
-        return super.onOptionsItemSelected(item);
+        // 加速度センサ
+        Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accSensor != null) {
+            sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        // GPS位置情報の取得（パーミッションチェック済み前提）
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, this);
+        }
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        locationManager.removeUpdates(this);
     }
+
+    // センサ値が変化したとき
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String msg = "";
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_LIGHT:
+                msg = "明るさ: " + event.values[0] + " lux";
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                msg = "加速度: X=" + event.values[0] + " Y=" + event.values[1] + " Z=" + event.values[2];
+                break;
+        }
+        textView.setText(msg);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // 精度変更時の処理（今回は何もしない）
+    }
+
+    // 位置が更新されたとき
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+        Toast.makeText(this, "位置: 緯度=" + lat + " 経度=" + lon, Toast.LENGTH_SHORT).show();
+    }
+
+    // その他の LocationListener メソッド
+    @Override public void onStatusChanged(String provider, int status, Bundle extras) {}
+    @Override public void onProviderEnabled(@NonNull String provider) {}
+    @Override public void onProviderDisabled(@NonNull String provider) {}
 }
